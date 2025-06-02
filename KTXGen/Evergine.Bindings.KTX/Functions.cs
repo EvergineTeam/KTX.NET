20c36f3a750e444044ef3386438a0f15d677e31a
Alejandro Juan Pérez <tuketet@gmail.com>
2025-06-02 12:50:01 +0200

    imprive handling of delegates

--- KTXGen/Evergine.Bindings.KTX/Functions.cs
+++ KTXGen/Evergine.Bindings.KTX/Functions.cs
@@ -12,41 +12,81 @@ namespace Evergine.Bindings.KTX
         [DllImport("ktx", CallingConvention = CallingConvention.Cdecl)]
         public static extern UInt32 ktxTexture_GetVkFormat(ktxTexture* This);
 
-        public static void ktxTexture_Destroy(ktxTexture* texture) => texture->vtbl->Destroy(texture);
+        public static void ktxTexture_Destroy(ktxTexture* texture)
+        {
+            var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXDESTROY>(texture->vtbl->Destroy);
+            f(texture);
+        }
 
         public static ktx_error_code_e ktxTexture_GetImageOffset(ktxTexture* texture, uint level, uint layer, uint faceOrSlice, out UIntPtr pOffset)
         {
             fixed (UIntPtr* pOffsetPtr = &pOffset)
             {
-                return texture->vtbl->GetImageOffset(texture, level, layer, faceOrSlice, pOffsetPtr);
+                var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXGETIMAGEOFFSET>(texture->vtbl->GetImageOffset);
+                return f(texture, level, layer, faceOrSlice, pOffsetPtr);
             }
         }
 
-        public static UIntPtr ktxTexture_GetDataSizeUncompressed(ktxTexture* texture) => texture->vtbl->GetDataSizeUncompressed(texture);
+        public static UIntPtr ktxTexture_GetDataSizeUncompressed(ktxTexture* texture)
+        {
+            var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXGETDATASIZEUNCOMPRESSED>(texture->vtbl->GetDataSizeUncompressed);
+            return f(texture);
+        }
 
-        public static UIntPtr ktxTexture_GetImageSize(ktxTexture* texture, uint level) => texture->vtbl->GetImageSize(texture, level);
-        public static UIntPtr ktxTexture_GetLevelSize(ktxTexture* texture, uint level) => texture->vtbl->GetLevelSize(texture, level);
+        public static UIntPtr ktxTexture_GetImageSize(ktxTexture* texture, uint level)
+        {
+            var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXGETIMAGESIZE>(texture->vtbl->GetImageSize);
+            return f(texture, level);
+        }
+        public static UIntPtr ktxTexture_GetLevelSize(ktxTexture* texture, uint level)
+        {
+            var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXGETLEVELSIZE>(texture->vtbl->GetLevelSize);
+            return f(texture, level);
+        }
 
-        public static ktx_error_code_e ktxTexture_IterateLevels(ktxTexture* texture, PFNKTXITERCB callback, void* userData) => texture->vtbl->IterateLevels(texture, callback, userData);
-        public static ktx_error_code_e ktxTexture_IterateLoadLevelFaces(ktxTexture* texture, PFNKTXITERCB callback, void* userData) => texture->vtbl->IterateLoadLevelFaces(texture, callback, userData);
+        public static ktx_error_code_e ktxTexture_IterateLevels(ktxTexture* texture, PFNKTXITERCB callback, void* userData)
+        {
+            var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXITERATELEVELS>(texture->vtbl->IterateLevels);
+            return f(texture, callback, userData);
+        }
 
-        public static bool ktxTexture_NeedsTranscoding(ktxTexture* texture) => texture->vtbl->NeedsTranscoding(texture) != 0;
+        public static ktx_error_code_e ktxTexture_IterateLoadLevelFaces(ktxTexture* texture, PFNKTXITERCB callback, void* userData)
+        {
+            var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXITERATELOADLEVELFACES>(texture->vtbl->IterateLoadLevelFaces);
+            return f(texture, callback, userData);
+        }
 
-        public static ktx_error_code_e ktxTexture_LoadImageData(ktxTexture* texture, byte* buffer, UIntPtr bufferSize) => texture->vtbl->LoadImageData(texture, buffer, bufferSize);
-        
+        public static bool ktxTexture_NeedsTranscoding(ktxTexture* texture)
+        {
+            var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXNEEDSTRANSCODING>(texture->vtbl->NeedsTranscoding);
+            return f(texture) != 0;
+        }
+
+        public static ktx_error_code_e ktxTexture_LoadImageData(ktxTexture* texture, byte* buffer, UIntPtr bufferSize)
+        {
+            var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXLOADIMAGEDATA>(texture->vtbl->LoadImageData);
+            return f(texture, buffer, bufferSize);
+        }
+
         public static ktx_error_code_e ktxTexture_SetImageFromMemory(ktxTexture* texture, uint level, uint layer, uint faceOrSlice, byte* buffer, UIntPtr bufferSize)
-            => texture->vtbl->SetImageFromMemory(texture, level, layer, faceOrSlice, buffer, bufferSize);
+        {
+            var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXSETIMAGEFROMMEMORY>(texture->vtbl->SetImageFromMemory);
+            return f(texture, level, layer, faceOrSlice, buffer, bufferSize);
+        }
+
         public static ktx_error_code_e ktxTexture_WriteToNamedFile(ktxTexture* texture, string filePath)
         {
             byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(filePath);
             fixed (byte* ptr = byteArray)
             {
-                return texture->vtbl->WriteToNamedFile(texture, ptr);
+                var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXWRITETONAMEDFILE>(texture->vtbl->WriteToNamedFile);
+                return f(texture, ptr);
             }
         }
         public static ktx_error_code_e ktxTexture_WriteToMemory(ktxTexture* texture, byte** bytes, UIntPtr* size)
         {
-                return texture->vtbl->WriteToMemory(texture, bytes, size);
+            var f = Marshal.GetDelegateForFunctionPointer<PFNKTEXWRITETOMEMORY>(texture->vtbl->WriteToMemory);
+            return f(texture, bytes, size);
         }
     }
 }
--- KTXGen/Evergine.Bindings.KTX/Structs.cs
+++ KTXGen/Evergine.Bindings.KTX/Structs.cs
@@ -15,20 +15,20 @@ namespace Evergine.Bindings.KTX
     [StructLayout(LayoutKind.Sequential)]
     public unsafe struct ktxTexture_vtbl
     {
-        public PFNKTEXDESTROY Destroy;
-		public PFNKTEXGETIMAGEOFFSET GetImageOffset;
-		public PFNKTEXGETDATASIZEUNCOMPRESSED GetDataSizeUncompressed;
-		public PFNKTEXGETIMAGESIZE GetImageSize;
-		public PFNKTEXGETLEVELSIZE GetLevelSize;
-		public PFNKTEXITERATELEVELS IterateLevels;
-		public PFNKTEXITERATELOADLEVELFACES IterateLoadLevelFaces;
-		public PFNKTEXNEEDSTRANSCODING NeedsTranscoding;
-		public PFNKTEXLOADIMAGEDATA LoadImageData;
-		public PFNKTEXSETIMAGEFROMMEMORY SetImageFromMemory;
+        public IntPtr Destroy;
+		public IntPtr GetImageOffset;
+		public IntPtr GetDataSizeUncompressed;
+		public IntPtr GetImageSize;
+		public IntPtr GetLevelSize;
+		public IntPtr IterateLevels;
+		public IntPtr IterateLoadLevelFaces;
+		public IntPtr NeedsTranscoding;
+		public IntPtr LoadImageData;
+		public IntPtr SetImageFromMemory;
 		public IntPtr SetImageFromStdioStream;
 		public IntPtr WriteToStdioStream;
-		public PFNKTEXWRITETONAMEDFILE WriteToNamedFile;
-		public PFNKTEXWRITETOMEMORY WriteToMemory;
+		public IntPtr WriteToNamedFile;
+		public IntPtr WriteToMemory;
 		public IntPtr WriteToStream;
 	}
 }
